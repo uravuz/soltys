e828124317fe1fc534528a01d9cfdeb0332fa6f9
uravuz <120816857+uravuz@users.noreply.github.com>
2026-05-29 09:29:06 +0200

    napraw oswiadczenie 2

--- components/Blocked.tsx
+++ components/Blocked.tsx
@@ -33,17 +33,17 @@ display: "block",
 marginBottom: "2px",
 textAlign: "center"
 }}>Z przykrością oświadczamy, że zażaleniem wychowawcy klasy 3TM zgłoszonym do opiekuna Samorządu Uczniowskiego, niezależny kandydat Bartosz Wojtkowiak został usunięty z list wyborczych.</p>
-p style={{
+<p style={{
 display: "block",
 marginBottom: "2px",
 textAlign: "center"
 }}>Jesteśmy oburzeni tą decyzją. Informacje o tym fakcie otrzymaliśmy dopiero w ostatni dzień szkolny przed wyborami do samorządu uczniowskiego.</p>
-p style={{
+<p style={{
 display: "block",
 marginBottom: "2px",
 textAlign: "center"
 }}>Decyzja jest dla nas niezwykle niezrozumiała, ponieważ wszystkie materiały wyborcze, przez które Bartosz został usunięty z listy wyborczej, zostały wcześniej zatwierdzone przez opiekuna Samorządu Uczniowskiego.</p>
-p style={{
+<p style={{
 display: "block",
 marginBottom: "24px",
 textAlign: "center"
